13097e28654ed40e5a2a75308c9b068244a99368
Zachary Jolly <zach.j.jolly@gmail.com>
2023-12-08 18:49:57 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,8 +8,8 @@ export const geoApiOptions = {
 
 export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
-export const WEATHER_API_KEY = "6fd10402011053133439086242243d8e";
+export const WEATHER_API_URL = "YOUR_API_KEY";
+export const WEATHER_API_KEY = "YOUR_API_KEY";
 
 
 
